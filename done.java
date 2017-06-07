5fc548c296893aadf9c8d84d5347364e53be9b53
Sasirekhamylsamy <sasirekhagokilavani0108@gmail.com>
2017-06-07 17:58:44 +0530

    Create done.java

---
+++ done.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class done
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+System.out.println("enter the num");
+int n=scan.nextInt();
+if(n%2==0)
+{
+System.out.println("the number is even");
+}
+else
+{
+System.out.println("the number is odd");
+}
+}
+}
